0f63b426253d168d767b05220204fdc82b9bd610
Ruan C. Keet <26340461@sun.ac.za>
2023-06-17 15:08:09 +0200

    Finished chapter 5: surface normals and multiple objects

--- src/EntryPoint.cpp
+++ src/EntryPoint.cpp
@@ -1,3 +1,4 @@
+#include "HitableList.hpp"
 #include "Image.hpp"
 #include "Sphere.hpp"
 
@@ -13,10 +14,15 @@ static glm::vec4 Lerp(const glm::vec4& from, const glm::vec4 to, f32 t)
     return (1.0f - t) * from + t * to;
 }
 
-static glm::vec4 GetColor(const Ray& ray, const Hitable& hitable)
+static glm::vec4 GetColor(const Ray& ray, const Hitable& world)
 {
-    if (hitable.Hit(ray))
-        return { 1.0f, 0.0f, 0.0f, 1.0f };
+    HitRecord record;
+
+    if (world.Hit(ray, 0.0f, __FLT_MAX__, record))
+    {
+        glm::vec4 color = { record.Normal.x + 1.0f, record.Normal.y + 1.0f, record.Normal.z + 1.0f, 2.0f /*This is for the multiply in the next line*/ };
+        return 0.5f * color;
+    }
         
     glm::vec3 unitDir = glm::normalize(ray.Direction);
     f32 t = 0.5f * unitDir.y + 1.0f;
@@ -31,7 +37,13 @@ int main()
 {
     Image image(WIDTH, HEIGHT);
 
-    Sphere sphere(glm::vec3(0.0f, 0.0f, -1.0f), 0.5f);
+    HitableList world;
+
+    Sphere sphere1(glm::vec3(0.0f, 0.0f, -1.0f), 0.5f);
+    Sphere sphere2(glm::vec3(0.0f, -100.5f, -1.0f), 100.0f);
+
+    world.Add(&sphere1);
+    world.Add(&sphere2);
 
     glm::vec3 lowerLeftCorner = { -2.0f, -1.0f, -1.0f };
     glm::vec3 horizontal      = {  4.0f,  0.0f,  0.0f };
@@ -47,7 +59,7 @@ int main()
 
             Ray ray = { origin, lowerLeftCorner + u * horizontal + v * vertical };
 
-            glm::vec4 color = GetColor(ray, sphere);
+            glm::vec4 color = GetColor(ray, world);
             image.SetColor(x, y, color); 
         }
     }
