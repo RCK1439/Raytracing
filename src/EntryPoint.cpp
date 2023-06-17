f2d8cf7a525a01ffb772ba68f8d0686d244089c0
Ruan C. Keet <26340461@sun.ac.za>
2023-06-17 13:15:51 +0200

    Now at chapter 4

--- src/EntryPoint.cpp
+++ src/EntryPoint.cpp
@@ -1,23 +1,45 @@
 #include "Image.hpp"
 
-#define WIDTH  400
-#define HEIGHT 400
+#include <glm/geometric.hpp>
+
+#define WIDTH  1024
+#define HEIGHT 512
+
+static glm::vec4 Lerp(const glm::vec4& from, const glm::vec4 to, f32 t)
+{
+    return (1.0f - t) * from + t * to;
+}
+
+static glm::vec4 GetColor(const rt::Ray& ray)
+{
+    glm::vec3 unitDir = glm::normalize(ray.Direction);
+    f32 t = 0.5f * unitDir.y + 1.0f;
+
+    glm::vec4 white = glm::vec4(1.0f);
+    glm::vec4 blue  = { 0.5f, 0.7f, 1.0f, 1.0f };
+
+    return Lerp(white, blue, t);
+}
 
 int main()
 {
     rt::Image image(WIDTH, HEIGHT);
 
+    glm::vec3 lowerLeftCorner = { -2.0f, -1.0f, -1.0f };
+    glm::vec3 horizontal      = {  4.0f,  0.0f,  0.0f };
+    glm::vec3 vertical        = {  0.0f,  2.0f,  0.0f };
+    glm::vec3 origin          = glm::vec3(0.0f);
+
     for (u32 y = 0; y < HEIGHT; y++)
     {
         for (u32 x = 0; x < WIDTH; x++)
         {
-            f32 r = (f32)x / (f32)WIDTH;
-            f32 g = (f32)y / (f32)HEIGHT;
-            f32 b = 0.2f;
-            f32 a = 1.0f;
+            f32 u = (f32)x / (f32)WIDTH;
+            f32 v = (f32)y / (f32)HEIGHT;
 
-            glm::vec4 color = { r, g, b, a };
+            rt::Ray ray = { origin, lowerLeftCorner + u * horizontal + v * vertical };
 
+            glm::vec4 color = GetColor(ray);
             image.SetColor(x, y, color); 
         }
     }
