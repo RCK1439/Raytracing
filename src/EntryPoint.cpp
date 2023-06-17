4f88f317ae786c93dc89849dcd53fccf7fd5ca8b
Ruan C. Keet <26340461@sun.ac.za>
2023-06-17 13:29:31 +0200

    Finished chapter 4

--- src/EntryPoint.cpp
+++ src/EntryPoint.cpp
@@ -1,17 +1,23 @@
 #include "Image.hpp"
+#include "Sphere.hpp"
 
 #include <glm/geometric.hpp>
 
 #define WIDTH  1024
 #define HEIGHT 512
 
+using namespace rt;
+
 static glm::vec4 Lerp(const glm::vec4& from, const glm::vec4 to, f32 t)
 {
     return (1.0f - t) * from + t * to;
 }
 
-static glm::vec4 GetColor(const rt::Ray& ray)
+static glm::vec4 GetColor(const Ray& ray, const Hitable& hitable)
 {
+    if (hitable.Hit(ray))
+        return { 1.0f, 0.0f, 0.0f, 1.0f };
+        
     glm::vec3 unitDir = glm::normalize(ray.Direction);
     f32 t = 0.5f * unitDir.y + 1.0f;
 
@@ -23,7 +29,9 @@ static glm::vec4 GetColor(const rt::Ray& ray)
 
 int main()
 {
-    rt::Image image(WIDTH, HEIGHT);
+    Image image(WIDTH, HEIGHT);
+
+    Sphere sphere(glm::vec3(0.0f, 0.0f, -1.0f), 0.5f);
 
     glm::vec3 lowerLeftCorner = { -2.0f, -1.0f, -1.0f };
     glm::vec3 horizontal      = {  4.0f,  0.0f,  0.0f };
@@ -37,9 +45,9 @@ int main()
             f32 u = (f32)x / (f32)WIDTH;
             f32 v = (f32)y / (f32)HEIGHT;
 
-            rt::Ray ray = { origin, lowerLeftCorner + u * horizontal + v * vertical };
+            Ray ray = { origin, lowerLeftCorner + u * horizontal + v * vertical };
 
-            glm::vec4 color = GetColor(ray);
+            glm::vec4 color = GetColor(ray, sphere);
             image.SetColor(x, y, color); 
         }
     }
