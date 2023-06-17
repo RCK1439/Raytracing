245acc683ca958626fab1a9c5895e1b578e96b3e
Ruan C. Keet <26340461@sun.ac.za>
2023-06-17 08:29:30 +0200

    Added code to test the image class

--- src/EntryPoint.cpp
+++ src/EntryPoint.cpp
@@ -1,7 +1,25 @@
-#include <iostream>
+#include "Image.hpp"
+
+#define WIDTH  400
+#define HEIGHT 400
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    rt::Image image(WIDTH, HEIGHT);
+
+    for (u32 y = 0; y < HEIGHT; y++)
+    {
+        for (u32 x = 0; x < WIDTH; x++)
+        {
+            f32 r = (f32)x / (f32)WIDTH;
+            f32 g = (f32)y / (f32)HEIGHT;
+            f32 b = 0.2f;
+
+            image.SetColorFloat(x, y, r, g, b); 
+        }
+    }
+
+    image.Save("out/test.png");
+
     return 0;
 }
