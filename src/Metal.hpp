ff1b1d1de9fb4c40d019b1ef3a0aad780f76eecd
Ruan C. Keet <26340461@sun.ac.za>
2023-06-18 11:39:40 +0200

    Added fuzz parameter to metal

--- src/Metal.hpp
+++ src/Metal.hpp
@@ -7,12 +7,13 @@ namespace rt
     class Metal final : public Material
     {
     public:
-        Metal(const glm::vec3& albedo);
+        Metal(const glm::vec3& albedo, f32 fuzz);
         ~Metal() override = default;
 
         bool Scatter(const Ray& ray, const HitRecord& record, glm::vec4& attenuation, Ray& scattered) const override;
 
     private:
         glm::vec4 m_Albedo;
+        f32 m_Fuzz = 0.0f;
     };
 } // namespace rt
