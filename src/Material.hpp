3889d2e31c5d0850053ae65e8c3a58b99b9e9b92
Ruan C. Keet <26340461@sun.ac.za>
2023-06-18 11:29:11 +0200

    Created material interface

---
+++ src/Material.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Types.hpp"
+
+#include <glm/vec4.hpp>
+
+namespace rt
+{
+    struct HitRecord;
+    
+    class Material
+    {
+    public:
+        virtual ~Material() = default;
+        virtual bool Scatter(const Ray& ray, const HitRecord& record, glm::vec4& attenuation, Ray& scattered) const = 0;
+    };
+} // namespace rt
