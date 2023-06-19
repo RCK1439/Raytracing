b7feea751ac11d86e5d455b3bbb6344ecb5c9c22
Ruan C. Keet <26340461@sun.ac.za>
2023-06-19 08:33:59 +0200

    Now deletes the materials in spheres

--- src/HitableList.cpp
+++ src/HitableList.cpp
@@ -1,16 +1,23 @@
 #include "HitableList.hpp"
+#include "Material.hpp"
 
 namespace rt
 {
+    HitableList::~HitableList()
+    {
+        for (auto& hitable : m_Data)
+            delete hitable.m_Material;
+    }
+
     bool HitableList::Hit(const Ray& ray, f32 tMin, f32 tMax, HitRecord& record) const
     {
         HitRecord tempRecord;
 
         bool hitAnything = false;
         f32 closest = tMax;
-        for (const auto* hitable : m_Data)
+        for (const auto& hitable : m_Data)
         {
-            if (hitable->Hit(ray, tMin, closest, tempRecord))
+            if (hitable.Hit(ray, tMin, closest, tempRecord))
             {
                 hitAnything = true;
                 closest = tempRecord.t;
@@ -21,7 +28,7 @@ namespace rt
         return hitAnything;
     }
 
-    void HitableList::Add(Hitable* hitable)
+    void HitableList::Add(const Sphere& hitable)
     {
         m_Data.push_back(hitable);
     }
